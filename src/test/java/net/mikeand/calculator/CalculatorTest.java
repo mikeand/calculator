f4835fc85e3baa8277e568842200880481fcc9fa
Michael Anderson <mikeand654@gmail.com>
2024-05-21 20:16:58 +1000

    Add sum logic, controller and unit test

---
+++ src/test/java/net/mikeand/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package net.mikeand.calculator;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+}
+
